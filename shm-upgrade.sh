95ac9ce5da54bda9fd269d436885f84edf02b85f
zycode08 <zycode08@gmail.com>
2023-04-14 22:35:54 -0500

    update

---
+++ shm-upgrade.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "===== Install Shardeum ====="
+#curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
+#echo "y"
+#echo "y"
+#echo "App147258."
+#echo -e"\n"
+#echo -e"\n"
+#echo -e"\n"
+curl -O https://raw.githubusercontent.com/zycode08/shardeum-script/main/installer.sh
+bash installer.sh
+
+docker-safe() {
+  if ! command -v docker &>/dev/null; then
+    echo "docker is not installed on this machine"
+    exit 1
+  fi
+
+  if ! docker $@; then
+    echo "Trying again with sudo..."
+    sudo docker $@
+  fi
+}
+
+docker-safe exec shardeum-dashboard operator-cli gui start
